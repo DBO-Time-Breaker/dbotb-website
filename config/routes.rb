5bf3031023419c6d20fe1bfac645fdfd4682c8e1
riagoncalves <ricardo.goncalves252@gmail.com>
2023-09-07 23:26:45 +0100

    feat(api): register endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope :api, module: :api do
+    post "register", to: "accounts#register"
+  end
 end
